2f64dd95b23e35a36a726fe3fc54d3b16d6640f9
SnoWolf2018 <574366448@qq.com>
2018-11-08 07:41:29 +0000

    commit modified 2.7/ptp.py

--- 2.7/ptp.py
+++ 2.7/ptp.py
@@ -2,19 +2,19 @@ import sys
 import os
 import numpy as np
 
-print 'current path'
-print os.getcwd()
-'''Your Shell Path'''
-print os.path.abspath(os.path.dirname(__file__))
-'''Your script Path'''
-
-print 'get upper path'
-print os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-print os.path.abspath(os.path.dirname(os.getcwd()))
-print os.path.abspath(os.path.join(os.getcwd(),".."))
-
-print 'get upper upper path'
-print os.path.abspath(os.path.join(os.getcwd(),"../.."))
+#print 'current path'
+#print os.getcwd()
+#'''Your Shell Path'''
+#print os.path.abspath(os.path.dirname(__file__))
+#'''Your script Path'''
+#
+#print 'get upper path'
+#print os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+#print os.path.abspath(os.path.dirname(os.getcwd()))
+#print os.path.abspath(os.path.join(os.getcwd(),".."))
+#
+#print 'get upper upper path'
+#print os.path.abspath(os.path.join(os.getcwd(),"../.."))
 
 '''Run everywhere in Archlinux PC'''
 #h,l=np.loadtxt('/home/archlinux/git/python/Code/ch3code/data.csv',delimiter=',',usecols=(4,5),unpack=True)
