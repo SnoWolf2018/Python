e7d5811a9a1ce9fc7c9c98b421562aff6db80746
SnoWolf2018 <574366448@qq.com>
2018-11-08 06:29:53 +0000

    modifie ptp.py relativepath

--- 2.7/ptp.py
+++ 2.7/ptp.py
@@ -1,5 +1,23 @@
+import sys
+import os
 import numpy as np
-h,l=np.loadtxt('/home/archlinux/git/python/Code/ch3code/data.csv',delimiter=',',usecols=(4,5),unpack=True)
+#h,l=np.loadtxt('/home/archlinux/git/python/Code/ch3code/data.csv',delimiter=',',usecols=(4,5),unpack=True)
+
+print 'current path'
+print os.getcwd()
+'''Your Shell Path'''
+print os.path.abspath(os.path.dirname(__file__))
+'''Your script Path'''
+
+print 'get upper path'
+print os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+print os.path.abspath(os.path.dirname(os.getcwd()))
+print os.path.abspath(os.path.join(os.getcwd(),".."))
+
+print 'get upper upper path'
+print os.path.abspath(os.path.join(os.getcwd(),"../.."))
+
+h,l=np.loadtxt('../Code/ch3code/data.csv',delimiter=',',usecols=(4,5),unpack=True)
 print "highest =",np.max(h)
 print "lowest =",np.min(l)
 print (np.max(h)+np.min(l))/2
