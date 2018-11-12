63561d67388e1842d4dcc3a2537226bd2a87dd1f
SnoWolf2018 <574366448@qq.com>
2018-11-12 12:06:33 +0800

    add day max/min analyze

--- 2.7/date.py
+++ 2.7/date.py
@@ -80,3 +80,16 @@ def datestr2num(s):
 '''Run anywhere in any Linux PC'''
 dates,close=np.loadtxt(filename,delimiter=',',usecols=(1,6),converters={1:datestr2num},unpack=True)
 print "Dates =",dates
+averages = np.zeros(5)
+for i in range(5):
+    indices = np.where(dates == i)
+    prices = np.take(close,indices)
+    avg = np.mean(prices)
+    print "Day",i,"prices",prices,"Average",avg
+    averages[i] = avg
+top = np.max(averages)
+print "Highest average",top
+print "Top day of the week",np.argmax(averages)
+bottom = np.min(averages)
+print "Lowest average",bottom
+print "Bottom day of the week",np.argmin(averages)
