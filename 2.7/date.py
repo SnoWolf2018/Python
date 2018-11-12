2458a0387087f363eac61824f962ca8169d4708d
SnoWolf2018 <574366448@qq.com>
2018-11-12 14:43:37 +0800

    in file 'date.py' add func summary

--- 2.7/date.py
+++ 2.7/date.py
@@ -78,7 +78,8 @@ from datetime import datetime
 def datestr2num(s):
     return datetime.strptime (s,"%d-%m-%Y").date().weekday()
 '''Run anywhere in any Linux PC'''
-dates,close=np.loadtxt(filename,delimiter=',',usecols=(1,6),converters={1:datestr2num},unpack=True)
+#dates,close=np.loadtxt(filename,delimiter=',',usecols=(1,6),converters={1:datestr2num},unpack=True)
+dates,open,high,low,close=np.loadtxt(filename,delimiter=',',usecols=(1,3,4,5,6),converters={1:datestr2num},unpack=True)
 print "Dates =",dates
 averages = np.zeros(5)
 for i in range(5):
@@ -93,3 +94,28 @@ print "Top day of the week",np.argmax(averages)
 bottom = np.min(averages)
 print "Lowest average",bottom
 print "Bottom day of the week",np.argmin(averages)
+
+#Get four week datas
+close = close[:16]
+dates = dates[:16]
+#Get first Monday
+first_monday = np.ravel(np.where(dates == 0))[0]
+print "The first Monday index is",first_monday
+#Get last Friday
+last_friday = np.ravel(np.where(dates == 4))[-1]
+print "The last Friday index is",last_friday
+
+weeks_indices = np.arange(first_monday,last_friday + 1)
+print "Weeks indices initial",weeks_indices
+weeks_indices = np.split(weeks_indices,3)
+print "Weeks indices after split",weeks_indices
+
+def summarize(a,o,h,l,c):
+    monday_open = o[a[0]]
+    week_high = np.max(np.take(h,a))
+    week_low = np.min(np.take(l,a))
+    friday_close = c[a[-1]]
+    return ("APPL",monday_open,week_high,week_low,friday_close)
+weeksummary = np.apply_along_axis(summarize,1,weeks_indices,open,high,low,close)
+print "Week weeksummary",weeksummary
+np.savetxt("Weeksummary.csv",weeksummary,delimiter=",",fmt="%s")
