1a01748f7727c53a91353cb0f279738f14586a74
SnoWolf2018 <574366448@qq.com>
2018-11-13 15:42:11 +0800

    add convolve.py

---
+++ 2.7/convolve.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import numpy as np
+
+script_path = os.path.abspath(os.path.dirname(__file__))
+parent_path = os.path.dirname(script_path)
+filename = parent_path+'/Code/ch3code/data.csv'
+
+from matplotlib.pyplot import plot
+from matplotlib.pyplot import show
+
+N = int(sys.argv[1])
+
+weights = np.ones(N)/N
+print "Weights",weights
+
+c=np.loadtxt(filename,delimiter=',',usecols=(6,),unpack=True)
+sma = np.convolve(weights,c)[N-1:-N+1]
+t = np.arange(N-1,len(c))
+plot(t,c[N-1:],lw=1.0)
+plot(t,sma,lw=2.0)
+show()
+
+
