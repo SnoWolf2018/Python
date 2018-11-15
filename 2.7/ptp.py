0420ed621dfb9a16bb7a440ca50f3a4df9150844
SnoWolf2018 <574366448@qq.com>
2018-11-15 10:15:48 +0800

    manual calc max min

--- 2.7/ptp.py
+++ 2.7/ptp.py
@@ -1,7 +1,9 @@
 import numpy as np
 h,l=np.loadtxt('/home/archlinux/git/python/Code/ch3code/data.csv',delimiter=',',usecols=(4,5),unpack=True)
-print "highest =",np.max(h)
-print "lowest =",np.min(l)
+print "highest h=",np.max(h)
+print "lowest h=",np.min(h)
+print "highest l=",np.max(l)
+print "lowest l=",np.min(l)
 print (np.max(h)+np.min(l))/2
 print "Spead high price",np.ptp(h)
 print "Spead low price",np.ptp(l)
